48c516bca275d2f89a20370a6b0abd5cb196b926
Jayrell Garcia <jay81423@gmail.com>
2025-01-28 17:33:21 -0600

    Folder name Update

---
+++ Homework #02 Polynomial Calculator/Homework #2 Polynomial Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework__2_Polynomial_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
