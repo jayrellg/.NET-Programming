1db227720e7a7af914693b5024ab855630a76fdc
Jayrell Garcia <jay81423@gmail.com>
2025-02-15 17:56:21 -0600

    HW 04 Push

---
+++ HW #04 Object-Oriented Programming/HW #04 Object-Oriented Programming/Program.cs
@@ -0,0 +1,70 @@
+﻿//Jayrell Garcia
+//SP25-CPSC-23000-001 - .NET Programming
+//This program lets the user create soccer player objects and save 3 properties to them (Name , position , and time played)
+
+
+namespace HW__04_Object_Oriented_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print header
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Welcome to the Soccer Player Tracker!");
+            Console.WriteLine("=====================================\n");
+
+
+            List<SoccerPlayer> players = new List<SoccerPlayer>();
+
+            //loop to ask user input to create SoccerPlayer objects
+            while (true)
+            {
+                //user input
+                Console.Write("Enter player name (or type 'quit' to stop): ");
+                string name = Console.ReadLine();
+                if (name.ToLower().Trim() == "quit") break;
+
+                Console.Write("Enter player position: ");
+                string position = Console.ReadLine().Trim();
+
+                Console.Write("Enter minutes played: ");
+                if (!int.TryParse(Console.ReadLine().Trim(), out int minutes))
+                {
+                    Console.WriteLine("Invalid input. Defaulting to 0 minutes.");
+                    minutes = 0;
+                }
+
+                //create Soccer Player object using user input
+                SoccerPlayer player = new SoccerPlayer(name, position, minutes);
+
+                // add the object to a list
+                players.Add(player);
+                Console.WriteLine("Player added successfully!\n");
+            }
+
+            // print Soccer Player objects in the order they were added
+            Console.WriteLine("\nPlayers Entered:");
+            players.ForEach(p => Console.WriteLine(p + "\n"));
+
+
+            // sort SoccerPlayer objects by minutes played and print again
+            List<SoccerPlayer> sortedSoccerPlayerByTime= players.OrderByDescending(p => p.MinutesPlayed).ToList();
+            Console.WriteLine("\nPlayers Sorted by Minutes Played:");
+            sortedSoccerPlayerByTime.ForEach(p => Console.WriteLine(p + "\n"));
+
+
+            Console.Write("\nEnter a position to filter players: ");
+            string filterPosition = Console.ReadLine().Trim();
+            List<SoccerPlayer> filteredPlayersByUserInput = players.Where(p => p.PlayerPosition.Equals(filterPosition, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (filteredPlayersByUserInput.Count > 0)
+                filteredPlayersByUserInput.ForEach(p => Console.WriteLine(p +"\n"));
+            else
+                Console.WriteLine($"No players found for position: {filterPosition}");
+
+
+
+        }
+    }
+}
---
+++ HW #04 Object-Oriented Programming/HW #04 Object-Oriented Programming/SoccerApp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW__04_Object_Oriented_Programming
+{
+    public class SoccerPlayer
+    {   
+        
+        //class properties
+        public string PlayerName { get; set; }
+        public string PlayerPosition { get; set; }
+       
+        private int minutesPlayed;
+        public int MinutesPlayed
+        {
+            get { return minutesPlayed; }
+            set {
+                if (value < 0)
+                    minutesPlayed = 0;
+                else
+                    minutesPlayed = value;
+                }
+        }
+
+
+        //default Constructor
+        public SoccerPlayer()
+        {
+            PlayerName = "Unknown";
+            PlayerPosition = "Unknown";
+            minutesPlayed = 0;
+        }
+
+        //non-default Constructor
+        public SoccerPlayer(string name, string position, int minutesPlayed )
+        {
+            PlayerName = name;
+            PlayerPosition = position;
+            MinutesPlayed = minutesPlayed;
+            
+        }
+
+        //calculate approx. how much distance they covred during the time they played in miles
+        public double calcDistanceCovered ()
+        {
+            return MinutesPlayed * 0.071111;
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return $"Player: {PlayerName}\n" +
+                   $"Position: {PlayerPosition}\n" +
+                   $"Minutes Played: {MinutesPlayed}\n" +
+                   $"Distance Covered: {calcDistanceCovered():F2} miles";
+        }
+
+    }
+
+
+
+
+}
